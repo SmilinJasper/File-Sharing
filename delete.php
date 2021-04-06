3d9b3c9c4fb51150b5f9e8246c0f91c0d058a09f
SmilinJasper <smilin.jasper@yahoo.com>
2021-04-07 01:31:27 +0530

    z-index-fixes-final-v-1.0

--- delete.php
+++ delete.php
@@ -5,7 +5,7 @@ unlink("uploads/{$_GET["name"]}");
 unlink("evaluation_forms/{$_GET["name"]}.html");
 
 //Connect to MySQL structure
-$con=mysqli_connect("localhost", "root", "", "student_database");
+$con=mysqli_connect("sql111.epizy.com", "epiz_28308908", "tq4nOlJirw", "epiz_28308908_student_database");
 
 // Check connection
 if (mysqli_connect_errno()) {
