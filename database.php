8b6afba65c9b76fccecc2e4d53d3714bee59a407
SmilinJasper <smilin.jasper@yahoo.com>
2021-07-02 23:20:15 +0530

    rename_database_to_server_database_name

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
 /* Attempt to connect to MySQL database */
-$conn = mysqli_connect("localhost","root","","online_paper_evaluation"); 
- 
+$conn = mysqli_connect("sql111.epizy.com", "epiz_28308908", "", "epiz_28308908_online_paper_evaluation");
+
 // Check connection
-if($conn === false){
+if ($conn === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
